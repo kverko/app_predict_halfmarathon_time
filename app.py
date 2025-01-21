4697e7cb64f891732507034c08db287c1367bbd2
Sławomir Rubiszewski <slawomirr@gmail.com>
2025-01-21 15:51:33 +0100

    get text and some imports

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
 
+st.set_page_config(page_title='Predict halfmarathon time', layout='centered')
 st.title("Predict halfmarathon time")
+
+st.session_state.setdefault("info", "")
+
+info = st.text_area(
+    label="I need some info to make the prediction",
+    placeholder="please tell me your age, gender "
+                "and how long it takes you to run 5km distance",
+    value=st.session_state["info"]
+)
+
+if st.button("send"):
+    st.write(info)
