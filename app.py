c1d3ef18ccf8230904dceba60975797aec7f7bba
Sławomir Rubiszewski <slawomirr@gmail.com>
2025-01-23 21:51:35 +0100

    send promt to openai

--- app.py
+++ app.py
@@ -1,4 +1,47 @@
+from dotenv import dotenv_values
+from openai import OpenAI
+import instructor
+from pydantic import BaseModel
 import streamlit as st
+env = dotenv_values(".env")
+
+openai_client = OpenAI(api_key=env["OPENAI_API_KEY"])
+
+instructor_openai_client = instructor.from_openai(openai_client)
+
+
+class Runner(BaseModel):
+    age: int
+    gender: str
+    run_time_5km: int
+    time_units: str
+
+
+def retrieve_structure(text, response_model):
+    st.write(text)
+    prompt = f"""
+        Extract following information from the provided text:
+        1. Age of the person, given in full years or None
+        2. Gender of the person: can be one of: "male", "female", None
+        3. Time required to run 5km given in seconds or None
+        4. Units of time required to run 5km: can be one of: 
+        "hours", "minutes", "seconds" or None
+        Here is the input text:
+        '{text}'
+        """
+
+    res = instructor_openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        temperature=0,
+        response_model=response_model,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ]
+    )
+    return res.model_dump()
 
 
 st.set_page_config(page_title='Predict halfmarathon time', layout='centered')
@@ -14,4 +57,5 @@ info = st.text_area(
 )
 
 if st.button("send"):
-    st.write(info)
+    resp = retrieve_structure(info, Runner)
+    st.write(resp)
